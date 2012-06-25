c9f6dc46794dd7e3bd2c7f7f853e3abb3a447b22
Thierry Goeckel <thierry@strayrayday.lu>
2012-06-26 01:54:35 +0200

    Fix 'undefined' errors.

--- resolvers/dilandau/content/contents/code/dilandau.js
+++ resolvers/dilandau/content/contents/code/dilandau.js
@@ -1,131 +1,118 @@
 /*
  * (c) 2011 lasconic <lasconic@gmail.com>
  * (c) 2011 leo franchi <lfranchi@kde.org>
+ * (c) 2012 thierry göckel <thierry@strayrayday.lu>
  */
 var DilandauResolver = Tomahawk.extend(TomahawkResolver, {
-    settings: {
-        name: 'Dilandau',
-        weight: 90,
-        timeout: 5,
-        strictMatch: true
-    },
-	parseISODuration: function( duration ) 
-	{
-		  var splitDurationRegex_ = new RegExp(
-		    '^(-)?P(?:(\\d+)Y)?(?:(\\d+)M)?(?:(\\d+)D)?' +
-		    '(T(?:(\\d+)H)?(?:(\\d+)M)?(?:(\\d+(?:\\.\\d+)?)S)?)?$');
-		  
-		  Tomahawk.log(typeof(duration));
-		  var parts = duration.match(splitDurationRegex_);
-		  if (!parts) {
-		    return null;
-		  }
-		
-		  var timeEmpty = !(parts[6] || parts[7] || parts[8]);
-		  var dateTimeEmpty = timeEmpty && !(parts[2] || parts[3] || parts[4]);
-		  if (dateTimeEmpty || timeEmpty && parts[5]) {
-		    return 0;
-		  }
-		
-		  var negative = parts[1];
-		  var years = parseInt(parts[2], 10) || 0;
-		  var months = parseInt(parts[3], 10) || 0;
-		  var days = parseInt(parts[4], 10) || 0;
-		  var hours = parseInt(parts[6], 10) || 0;
-		  var minutes = parseInt(parts[7], 10) || 0;
-		  var seconds = parseFloat(parts[8]) || 0;
-		  return negative ? (-days * 24 * 3600 - hours * 3600 - minutes*60 -seconds) :
-		                    (days * 24 * 3600 + hours * 3600 + minutes*60 + seconds);
-		
+
+	settings: {
+		name: 'Dilandau',
+		weight: 90,
+		timeout: 10
 	},
-    handleResponse: function(qid, artist, album, title) {
-        var that = this;
-        return function (xhr) {
-            var matches = [];
-            var xmlString = xhr.responseText;
+
+	handleResponse: function(qid, artist, album, title) {
+		var that = this;
+		return function (xhr) {
+			var matches = [];
+			var xmlString = xhr.responseText;
 			xmlString.replace(/<a class="button tip download_button" title="[^"]*"  href="([^"]*)"/g, function() {
 				matches.push(Array.prototype.slice.call(arguments,1,2));
 			});
-			
 			var matchesTitle = [];
 			xmlString.replace(/<h2 class="title_song" title="([^"]*)"/g, function() {
 				matchesTitle.push(Array.prototype.slice.call(arguments,1,2));
 			});
-			
-			//TODO duration are not yet accurate on dilandau
-			//var matchesDuration = [];
-			//xmlString.replace(/<meta itemprop="duration" content="([^"]*)"/g, function() {
-			//	matchesDuration.push(Array.prototype.slice.call(arguments,1,2));
-			//});
+			var results = [];
+			var empty = {
+				qid: qid,
+				results: []
+			};
+			if (matches.length > 0 && matches.length === matchesTitle.length) {
+				var stop = matches.length;
+				for (var i = 0; i < matches.length; i++) {
+					var url = matches[i][0];
+					var dTitle = matchesTitle[i];
+					var dTitleLower = dTitle.toString().toLowerCase();
+					if (dTitleLower.indexOf(artist.toLowerCase()) === -1 || dTitleLower.indexOf(title.toLowerCase()) === -1) {
+						stop = stop - 1;
+						continue;
+					}
+					else {
+						(function(qid, url) {
+							var http = new XMLHttpRequest();
+							http.open('HEAD', url, true);
+							http.onreadystatechange = function() {
+								if (http.readyState === 4){
+									if (http.status === 200 && (http.getResponseHeader("Content-Type").indexOf("audio") !== -1 || http.getResponseHeader("Content-Type").indexOf("video") !== -1 )){
+										var result = {};
+										result.artist = artist;
+										result.track = title;
+										result.source = that.settings.name;
+										result.url = url;
+										result.mimetype = "audio/mpeg";
+										result.bitrate = 128;
+										result.score = 0.9;
+										results.push(result);
+										stop = stop - 1;
+										if (stop === 0){
+											var return1 = {
+												qid: qid,
+												results: [results[0]]
+											};
+											Tomahawk.addTrackResults(return1);
+										}
+									}
+									else {
+										stop = stop - 1;
+									}
+								}
+							};
+							http.send(null);
+						})(qid, url);
+					}
+				}
+				if (stop === 0){
+					Tomahawk.addTrackResults(empty);
+				}
+			}
+			else {
+				Tomahawk.addTrackResults(empty);
+			}
+		};
+	},
 
-            var results = [];
-            if (matches.length > 0 && matches.length == matchesTitle.length) {
-                // walk through the results and store it in 'results'
-                for (var i = 0; i < matches.length; i++) {
-                    var link = matches[i];
-                    var dTitle = matchesTitle[i];
-                    var dTitleLower = dTitle.toString().toLowerCase();
-                    
-                   	var dDuration = 0;
-		            //TODO Currently dilandau duration are not accurate.
-		            //if(matchesDuration.length == matchesTitle.length) {
-		            	//Tomahawk.log(matchesDuration[i]);
-		            	//dDuration = this.parseISODuration(matchesDuration[i].toString());
-		            //}
-		            
-                    if (!that.settings.strictMatch || (dTitleLower.indexOf(artist.toLowerCase()) !== -1 && dTitleLower.indexOf(title.toLowerCase()) !== -1)) {
-                        var result = {};
-                        result.artist = artist;
-                        result.album = album;
-                        result.track = title;
-                        result.duration - dDuration;
 
-                        result.source = that.settings.name;
-                        result.url = decodeURI(link);
+	resolve: function (qid, artist, album, title) {
+		// build query to Dilandau
+		var url = "http://www.dilandau.eu/download_music/";
+		var request = "";
+		if (title !== ""){
+			request += title.replace(/ /g, '-');
+		}
 
-                        result.mimetype = "audio/mp3";
-                        result.bitrate = 128;
-                        if (that.settings.strictMatch) result.score = 1.0;
-                        else result.score = 0.5;
+		if (artist !== "") {
+			if (title !== ""){
+				request += '-';
+			}
+			request += artist.replace(/ /g, '-');
+		}
 
-                        results.push(result);
-                    }
-                }
-            }
-            var return1 = {
-                qid: qid,
-                results: results
-            };
-            Tomahawk.addTrackResults( return1 );
-       }
-    },
+		url += encodeURIComponent(request);
 
-    resolve: function (qid, artist, album, title) {
-        // build query to Dilandau
-        var url = "http://www.dilandau.eu/download_music/";
-        var request = "";
-        if (title !== "") request += title.replace(/ /g, '-');
+		url += "-1.html";
 
-        if (artist !== "") {
-            if (title !== "") request += '-';
-            request += artist.replace(/ /g, '-');
-        }
-
-        url += encodeURIComponent(request)
-
-        url += "-1.html";
-
-        // send request and parse it into javascript
-        Tomahawk.asyncRequest(url, this.handleResponse(qid, artist, album, title));
-    },
+		// send request and parse it into javascript
+		Tomahawk.asyncRequest(url, this.handleResponse(qid, artist, album, title));
+	},
     
-    search: function (qid, searchString) {
-        var return1 = {
-            qid: qid,
-            results: new Array()
-        };
-        return return1;
-    }
+	search: function (qid, searchString) {
+		var return1 = {
+			qid: qid,
+			results: new Array()
+		};
+		return return1;
+	}
 });
 
 Tomahawk.resolver.instance = DilandauResolver;
