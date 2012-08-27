1c0c28c579bac2b4cdc372e5654af313c555eed2
Leo Franchi <lfranchi@kde.org>
2012-08-27 08:54:58 -0400

    Version 1.0 of the officialfm resolver, using the v2 api

--- resolvers/officialfm/content/contents/code/officialfm.js
+++ resolvers/officialfm/content/contents/code/officialfm.js
@@ -1,77 +1,119 @@
-/*
- * (c) 2011 lasconic <lasconic@gmail.com>
- */
 var OfficialfmResolver = Tomahawk.extend(TomahawkResolver, {
     settings: {
         name: 'Official.fm',
         weight: 70,
         timeout: 5
     },
-    process: function (qid, artist, album, title, strict) {
-        var applicationKey = "ixHOUAG9r9csybvGtGuf";
+    
+    spell: function(a){magic=function(b){return(b=(b)?b:this).split("").map(function(d){if(!d.match(/[A-Za-z]/)){return d}c=d.charCodeAt(0)>=96;k=(d.toLowerCase().charCodeAt(0)-96+12)%26+1;return String.fromCharCode(k+(c?96:64))}).join("")};return magic(a)},
 
-        var valueForSubNode = function (node, tag) {
-                return node.getElementsByTagName(tag)[0].textContent;
-            };
+    init: function () {
+        this.secret = this.spell("yptuKlFHC3azLLcBNYoCHW6t30I1M5uy");
+    },
+    
+    asyncRequest: function (url, callback) {
+        var xmlHttpRequest = new XMLHttpRequest();
+        xmlHttpRequest.open('GET', url, true);
+        xmlHttpRequest.setRequestHeader('X-Api-Version', 2.0);
+	Tomahawk.log("Doing API call: " + url);
+        xmlHttpRequest.onreadystatechange = function () {
+            if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
+                callback.call(window, xmlHttpRequest);
+            } else if (xmlHttpRequest.readyState === 4) {
+                Tomahawk.log("Failed to do GET request: to: " + url);
+                Tomahawk.log("Status Code was: " + xmlHttpRequest.status);
+            }
+        }
+        xmlHttpRequest.send(null);
+    },
 
-        // build query to Official.fm
-        var url = "http://api.official.fm/search/tracks/";
-        var request = "";
-        if (title !== "") request += title + " ";
-
-        if (artist !== "") request += artist + " ";
-
-        url += encodeURIComponent(request);
-
-        url += "?key=" + applicationKey;
-        Tomahawk.log(url);
-        // send request and parse it into javascript
+    resolve: function (qid, artist, album, title) {
+        if (artist !== "") {
+            query = encodeURIComponent(artist) + "+";
+        }
+        if (title !== "") {
+            query += encodeURIComponent(title);
+        }
+        var apiQuery = "http://api.official.fm/tracks/search?api_key=" + this.secret + "api_version=2.0&q=" + query;
         var that = this;
-        Tomahawk.asyncRequest(url, function(xhr) {
-            // parse xml
-            var domParser = new DOMParser();
-            xmlDoc = domParser.parseFromString(xhr.responseText, "text/xml");
+        var resultObj = {
+            results: [],
+            qid: qid
+        };
+        that.asyncRequest(apiQuery, function (xhr) {
+            var resp = JSON.parse(xhr.responseText);
+            if (resp.total_entries !== 0) {
+                for (var i = 0; i < Math.min(3, resp.total_entries); i++) {
+                    if (resp.tracks[i] === undefined || resp.tracks[i].track === undefined) {
+                        continue;
+                    }
+                    var track = resp.tracks[i].track;
 
-            var results = new Array();
-            var r = xmlDoc.getElementsByTagName("tracks");
-            // check the response
-            if (r.length > 0 && r[0].childNodes.length > 0) {
-                var links = xmlDoc.getElementsByTagName("track");
+                    Tomahawk.log("Result: " + JSON.stringify(track));
+                    if (track.streaming === undefined || track.streaming.http === undefined) {
+                        Tomahawk.log("Found result from Official.fm but no streaming url...");
+                        continue;
+                    }
 
-                // walk through the results and store it in 'results'
-                for (var i = 0; i < links.length; i++) {
-                    var link = links[i];
-                    var result = new Object();
-                    result.artist = valueForSubNode(link, "artist_string");
-                    result.album = album;
-                    result.track = valueForSubNode(link, "title");
+                    var result = {
+                        track: track.title,
+                        artist: track.artist
+                    };
 
                     result.source = that.settings.name;
-                    result.duration = valueForSubNode(link, "length");
-                    result.score = 0.95;
-                    result.id = valueForSubNode(link, "id");
-                    if (!strict || (result.artist == artist && result.track == title)) {
-                        var urlStream = 'http://api.official.fm/track/' + result.id + '/stream?key=' + applicationKey + "&format=json";
+                    result.mimetype = "audio/mpeg";
+                    result.bitrate = 160;
+                    result.duration = track.duration;
+                    result.score = 0.85;
+                    result.url = track.streaming.http;
 
-                        var t = JSON.parse(Tomahawk.syncRequest(urlStream));
-                        result.url = t.stream_url;
-                        result.mimetype = "audio/mpeg";
-                        results.push(result);
-                    }
+                    resultObj.results.push(result);
                 }
             }
-            var return1 = {
-                qid: qid,
-                results: results
-            };
-            Tomahawk.addTrackResults(return1);
+            Tomahawk.addTrackResults(resultObj);
         });
     },
-    resolve: function (qid, artist, album, title) {
-        this.process(qid, artist, album, title, true);
-    },
+
     search: function (qid, searchString) {
-        this.process(qid, "", "", searchString, false);
+        var apiQuery = "http://api.official.fm/tracks/search?api_key=" + this.secret + "api_version=2.0&q=" + encodeURIComponent(searchString.replace('"', '').replace("'", ""));
+        var that = this;
+        var resultObj = {
+            results: [],
+            qid: qid
+        };
+        this.asyncRequest(apiQuery, function (xhr) {
+            var resp = JSON.parse(xhr.responseText);
+            if (resp.total_entries !== 0) {
+                for (var i = 0; i < resp.total_entries; i++) {
+                    if (resp.tracks[i] === undefined || resp.tracks[i].track === undefined) {
+                        continue;
+                    }
+                    var track = resp.tracks[i].track;
+
+                    Tomahawk.log("Result: " + JSON.stringify(track));
+                    if (track.streaming === undefined || track.streaming.http === undefined) {
+                        Tomahawk.log("Found result from Official.fm but no streaming url...");
+                        continue;
+                    }
+
+                    var result = {
+                        track: track.title,
+                        artist: track.artist
+                    };
+
+                    result.source = that.settings.name;
+                    result.mimetype = "audio/mpeg";
+                    result.bitrate = 160;
+                    result.duration = track.duration;
+                    result.score = 0.85;
+                    result.url = track.streaming.http;
+
+                    resultObj.results.push(result);
+                }
+            }
+            Tomahawk.addTrackResults(resultObj);
+
+        });
     }
 });
 
