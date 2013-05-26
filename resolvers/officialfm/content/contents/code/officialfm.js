7e3b0b5c689212800c97b9ca735fc48baeebe472
Uwe L. Korn <uwelk@xhochy.com>
2013-05-26 18:35:59 +0200

    Update Official.fm to 1.0.6

--- resolvers/officialfm/content/contents/code/officialfm.js
+++ resolvers/officialfm/content/contents/code/officialfm.js
@@ -1,16 +1,36 @@
+/* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
+ *
+ *   Copyright 2011, lasconic <lasconic@gmail.com>
+ *   Copyright 2011, Leo Franchi <lfranchi@kde.org>
+ *
+ *   Tomahawk is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tomahawk is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var OfficialfmResolver = Tomahawk.extend(TomahawkResolver, {
     settings: {
         name: 'Official.fm',
+        icon: 'officialfm-icon.png',
         weight: 70,
         timeout: 5
     },
-    
+
     spell: function(a){magic=function(b){return(b=(b)?b:this).split("").map(function(d){if(!d.match(/[A-Za-z]/)){return d}c=d.charCodeAt(0)>=96;k=(d.toLowerCase().charCodeAt(0)-96+12)%26+1;return String.fromCharCode(k+(c?96:64))}).join("")};return magic(a)},
 
     init: function () {
         this.secret = this.spell("yptuKlFHC3azLLcBNYoCHW6t30I1M5uy");
     },
-    
+
     asyncRequest: function (url, callback) {
         var xmlHttpRequest = new XMLHttpRequest();
         xmlHttpRequest.open('GET', url, true);
@@ -66,6 +86,7 @@ var OfficialfmResolver = Tomahawk.extend(TomahawkResolver, {
                     result.duration = track.duration;
                     result.score = 0.85;
                     result.url = track.streaming.http;
+                    result.checked = true;
 
                     resultObj.results.push(result);
                 }
@@ -107,6 +128,7 @@ var OfficialfmResolver = Tomahawk.extend(TomahawkResolver, {
                     result.duration = track.duration;
                     result.score = 0.85;
                     result.url = track.streaming.http;
+                    result.checked = true;
 
                     resultObj.results.push(result);
                 }
