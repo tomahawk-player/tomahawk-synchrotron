1d47c5d14f234d7ae675a407c7b13ddb4806617f
Dominik Schmidt <domme@tomahawk-player.org>
2016-01-28 00:16:04 +0100

    update beets

--- resolvers/beets/content/contents/code/beets.js
+++ resolvers/beets/content/contents/code/beets.js
@@ -18,103 +18,15 @@
  *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// Map all the audio types supported by beets to extensions and MIME types.
-var AUDIO_TYPES = {
-    'MP3':      ['mp3',  'audio/mpeg'],
-    'AAC':      ['m4a',  'audio/mp4'],
-    'OGG':      ['ogg',  'audio/ogg'],
-    'FLAC':     ['flac', 'audio/x-flac'],
-    'APE':      ['ape',  'audio/ape'],
-    'WavPack':  ['wv',   'audio/x-wavpack'],
-    'MusePack': ['mpc',  'audio/x-musepack']
-};
+var BeetsResolver = Tomahawk.extend(Tomahawk.Resolver, {
 
-// Backward compability for Tomahawk<0.7.100/API<0.2.0 which did not support authed requests
-if (Tomahawk.hasOwnProperty('apiVersion') && Tomahawk.atLeastVersion('0.2.0')) {
-    var passwordRequest = function (url, username, password, cb, errorHandler) {
-        Tomahawk.asyncRequest(url, cb, {}, {username: username, password: password, errorHandler: errorHandler});
-    };
-} else {
-    var passwordRequest = function (url, username, password, cb, errorHandler) {
-        var xmlHttpRequest = new XMLHttpRequest();
-        xmlHttpRequest.open('GET', url, true, username, password);
-        xmlHttpRequest.onreadystatechange = function() {
-            if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
-                cb(xmlHttpRequest);
-            } else if (xmlHttpRequest.readyState == 4) {
-                errorHandler(xmlHttpRequest);
-            }
-        };
-        xmlHttpRequest.send(null);
-    };
-}
+    apiVersion: 0.9,
 
-var BeetsResolver = Tomahawk.extend(TomahawkResolver, {
-    trackCount: -1,
     settings: {
         name: 'beets',
         icon: 'beets-icon.png',
         weight: 95,
         timeout: 5
-    },
-
-    // Resolution.
-    resolve: function (qid, artist, album, title) {
-        if (album == '') {
-            this.beetsQuery(qid, ['artist:' + artist, 'title:' + title]);
-        } else {
-            this.beetsQuery(qid, ['artist:' + artist, 'album:' + album, 'title:' + title]);
-        }
-    },
-
-    search: function (qid, searchString) {
-        this.beetsQuery(qid, searchString.split(' '));
-    },
-
-    baseUrl: function () {
-        if (this.useTLS) {
-            return 'https://' + this.host + ':' + this.port;
-        } else {
-            return 'http://' + this.host + ':' + this.port;
-        }
-    },
-
-    beetsQuery: function (qid, queryParts) {
-        var baseUrl = this.baseUrl(),
-            url = this.baseUrl() + '/item/query/';
-        queryParts.forEach(function (item) {
-            url += encodeURIComponent(item);
-            url += '/';
-        });
-        url = url.substring(0, url.length - 1);  // Remove last /.
-
-        passwordRequest(url, this.username, this.password, function (xhr) {
-            var resp = JSON.parse(xhr.responseText),
-            items = resp.results,
-            searchResults = [];
-            items.forEach(function (item) {
-                var type_info = AUDIO_TYPES[item.format];
-                searchResults.push({
-                    artist: item.artist,
-                    album: item.album,
-                    track: item.title,
-                    albumpos: item.track,
-                    source: "beets",
-                    url: baseUrl + '/item/' + item.id + '/file',
-                    bitrate: Math.floor(item.bitrate / 1024),
-                    duration: Math.floor(item.length),
-                    size: (item.size || 0),
-                    score: 1.0,
-                    extension: type_info[0],
-                    mimetype: type_info[1],
-                    year: item.year
-                });
-            });
-            Tomahawk.addTrackResults({
-                qid: qid,
-                results: searchResults
-            });
-        });
     },
 
     // Configuration.
@@ -123,21 +35,9 @@ var BeetsResolver = Tomahawk.extend(TomahawkResolver, {
         return {
             "widget": uiData,
             "fields": [{
-                name: "host",
-                widget: "hostField",
+                name: "server",
+                widget: "serverField",
                 property: "text"
-            }, {
-                name: "port",
-                widget: "portField",
-                property: "text"
-            }, {
-                name: "useTLS",
-                widget: "tlsCheckBox",
-                property: "checked"
-            },{
-                name: "useAuth",
-                widget: "useAuthCheckBox",
-                property: "checked"
             }, {
                 name: "username",
                 widget: "usernameField",
@@ -149,158 +49,176 @@ var BeetsResolver = Tomahawk.extend(TomahawkResolver, {
             }]
         };
     },
-    newConfigSaved: function () {
-        this.init();
-    },
-    init: function () {
-        var userConfig = this.getUserConfig(),
-            that = this;
-        this.host = userConfig.host || 'localhost';
-        this.port = parseInt(userConfig.port, 10);
-        this.useTLS = userConfig.useTLS;
-        this.useAuth = userConfig.useAuth;
-        if (this.useAuth) {
-            this.username = userConfig.username;
-            this.password = userConfig.password;
-        } else {
-            this.username = null;
-            this.password = null;
-        }
-        if (isNaN(this.port) || !this.port) {
-            this.port = 8337;
-        }
-        // Invalidate trackCount
-        // Check if /stats is available and we can get enough information for ScriptCollection support and track count
-        // (this works for beets 1.2.1+)
-        passwordRequest(this.baseUrl() + '/stats', this.username, this.password, function (xhr) {
-            // Success
-            that.trackCount = parseInt(JSON.parse(xhr.responseText).items);
-            Tomahawk.reportCapabilities(TomahawkResolverCapability.Browsable);
-        }, function (xhr) {
-            // Failed
-            that.trackCount = -1;
-            // Check if /artist/ is available and we can get enough information for ScriptCollection support
-            // (this is needed for beets 1.1.0-1.2.0)
-            passwordRequest(that.baseUrl() + '/artist/', this.username, this.password, function () {
-                // Success
-                Tomahawk.reportCapabilities(TomahawkResolverCapability.Browsable);
-            }, function () {
-                // Failed
-                Tomahawk.reportCapabilities(TomahawkResolverCapability.NullCapability);
-            });
+
+    newConfigSaved: function (newConfig) {
+        console.log("Invalidating cache");
+        var that = this;
+        beetsCollection.wipe({id: beetsCollection.settings.id}).then(function () {
+            window.localStorage.removeItem("beets_trackCount");
+            window.localStorage.removeItem("beets_albumCount");
+            that.init();
         });
     },
 
-    configTest: function () {
-        Tomahawk.asyncRequest(this.baseUrl(),
-            function (xhr) {
-                Tomahawk.onConfigTestResult(TomahawkConfigTestResultType.Success);
-            }, {}, {
-                method: 'HEAD',
-                username: this.username,
-                password: this.password,
-                errorHandler: function (xhr) {
-                    if (xhr.status == 403) {
-                        Tomahawk.onConfigTestResult(TomahawkConfigTestResultType.InvalidCredentials);
-                    } else if (xhr.status == 404 || xhr.status == 0) {
-                        Tomahawk.onConfigTestResult(TomahawkConfigTestResultType.CommunicationError);
-                    } else {
-                        Tomahawk.onConfigTestResult(TomahawkConfigTestResultType.Other,
-                            xhr.responseText.trim());
-                    }
-                }
+    _sanitizeConfig: function (config) {
+        if (!config.server) {
+            config.server = "http://localhost:8337/";
+        } else {
+            if (config.server.search("^.*:\/\/") < 0) {
+                // couldn't find a proper protocol, so we default to "http://"
+                config.server = "http://" + config.server;
             }
-        );
-    },
 
-    // Script Collection Support
-
-    artists: function (qid) {
-        var url = this.baseUrl() + '/artist/';
-        passwordRequest(url, this.username, this.password, function (xhr) {
-            var response = JSON.parse(xhr.responseText);
-            Tomahawk.addArtistResults({
-                qid: qid,
-                artists: response.artist_names
-            });
-        });
-    },
-
-    albums: function (qid, artist) {
-        var url = this.baseUrl() + '/album/query/albumartist:' + encodeURIComponent(artist);
-        passwordRequest(url, this.username, this.password, function (xhr) {
-            var response = JSON.parse(xhr.responseText),
-            results = [];
-            response.results.forEach(function (item) {
-                results.push(item.album);
-            });
-            Tomahawk.addAlbumResults({
-                qid: qid,
-                artist: artist,
-                albums: results
-            });
-        });
-    },
-
-    tracks: function (qid, artist, album) {
-        var url = this.baseUrl() + '/item/query/' + encodeURIComponent('artist:' + artist) + '/' + encodeURIComponent('album:' + album);
-        var baseUrl = this.baseUrl();
-        passwordRequest(url, this.username, this.password, function (xhr) {
-            var response = JSON.parse(xhr.responseText),
-            searchResults = [];
-            response.results.forEach(function (item) {
-                var type_info = AUDIO_TYPES[item.format];
-                searchResults.push({
-                    artist: item.artist,
-                    album: item.album,
-                    track: item.title,
-                    albumpos: item.track,
-                    source: "beets",
-                    url: baseUrl + '/item/' + item.id + '/file',
-                    bitrate: Math.floor(item.bitrate / 1024),
-                    duration: Math.floor(item.length),
-                    size: (item.size || 0),
-                    score: 1.0,
-                    extension: type_info[0],
-                    mimetype: type_info[1],
-                    year: item.year
-                });
-            });
-            searchResults.sort(function (a, b) {
-                if (a.albumpos < b.albumpos) {
-                    return -1;
-                } else if (a.albumpos > b.albumpos) {
-                    return 1;
-                } else {
-                    return 0;
+            // qtwebkit doesn't support toString() or href on URLs
+            if(URL.prototype.hasOwnProperty('toString')) {
+                var url = new URL(config.server);
+                if (!url.port) {
+                    url.port = 8337;
                 }
-            });
-            Tomahawk.addAlbumTrackResults({
-                qid: qid,
-                artist: artist,
-                album: album,
-                results: searchResults
-            });
+                config.server = url.toString();
+            }
+        }
+
+        return config;
+    },
+
+    init: function () {
+        console.log('INIT', this.getUserConfig());
+        var config = this._sanitizeConfig(this.getUserConfig());
+        console.log('INIT2', config);
+        this._server = config.server;
+        this._username = config.username;
+        this._password = config.password;
+
+        this._ensureCollection();
+    },
+
+    _ensureCollection: function () {
+        var that = this;
+
+        return beetsCollection.revision({
+            id: beetsCollection.settings.id
+        }).then(function (result) {
+            var lastCollectionUpdate = window.localStorage["beets_last_collection_update"];
+            if (lastCollectionUpdate && lastCollectionUpdate == result) {
+                console.log("Collection database has not been changed since last time.");
+                return that._fetchAndStoreCollection();
+            } else {
+                console.log("Collection database has been changed. Wiping and re-fetching...");
+                window.localStorage.removeItem("beets_trackCount");
+                window.localStorage.removeItem("beets_albumCount");
+                return beetsCollection.wipe({
+                    id: beetsCollection.settings.id
+                }).then(function () {
+                    return that._fetchAndStoreCollection();
+                });
+            }
         });
     },
 
-    collection: function () {
-        if (this.trackCount > -1) {
-            return {
-                prettyname: "Beets",
-                description: this.host,
-                iconfile: 'beets-icon.png',
-                trackcount: this.trackCount
-            };
-        } else {
-            // We cannot get the trackcout for this collection
-            return {
-                prettyname: "Beets",
-                description: this.host,
-                iconfile: 'beets-icon.png'
+    _fetchAndStoreCollection: function () {
+        var that = this;
+
+        var settings;
+        if (this._username && this._password) {
+            settings = {
+                username: this._username,
+                password: this._password
             };
         }
+
+        Tomahawk.get(this._server + 'stats', settings).then(function (response) {
+            var trackCount = parseInt(response.items);
+            var albumCount = parseInt(response.albums);
+            Tomahawk.PluginManager.registerPlugin("collection", beetsCollection);
+            if (window.localStorage["beets_trackCount"] != trackCount
+                || window.localStorage["beets_albumCount"] != albumCount) {
+                var msg = "";
+                if (window.localStorage["beets_trackCount"] != trackCount) {
+                    msg += "Track count has changed from " + window.localStorage["beets_trackCount"]
+                        + " to " + trackCount + ". ";
+                }
+                if (window.localStorage["beets_albumCount"] != albumCount) {
+                    msg += "Album count has changed from " + window.localStorage["beets_albumCount"]
+                        + " to " + albumCount + ". ";
+                }
+                console.log(msg + "Updating collection ...");
+                return Tomahawk.get(that._server + "item", settings).then(function (response) {
+                    console.log("RESPONSE: ", response);
+                    var searchResults = [];
+                    response.items.forEach(function (item) {
+                        searchResults.push({
+                            artist: item.artist,
+                            artistDisambiguation: "",
+                            albumArtist: item.artist,
+                            albumArtistDisambiguation: "",
+                            album: item.album,
+                            track: item.title,
+                            albumpos: item.track,
+                            url: that._server + 'item/' + item.id + '/file',
+                            duration: Math.floor(item.length)
+                        });
+                    });
+                    console.log("wipe collection");
+                    beetsCollection.wipe({id: beetsCollection.settings.id}).then(function () {
+                        console.log("wiped collection");
+                        beetsCollection.addTracks({
+                            id: beetsCollection.settings.id,
+                            tracks: searchResults
+                        }).then(function (newRevision) {
+                            console.log("tracks added");
+                            window.localStorage["beets_trackCount"] = trackCount;
+                            window.localStorage["beets_albumCount"] = albumCount;
+                            window.localStorage["beets_last_collection_update"] = newRevision;
+                        });
+                    });
+                });
+            } else {
+                console.log("Track count is still " + trackCount
+                    + ". Album count is still " + albumCount
+                    + ". No collection update necessary.");
+                beetsCollection.addTracks({
+                    id: beetsCollection.settings.id,
+                    tracks: []
+                });
+            }
+        });
+
+    },
+
+    testConfig: function (config) {
+        config = this._sanitizeConfig(config);
+
+        var settings;
+        if (config.username && config.password) {
+            settings = {
+                username: config.username,
+                password: config.password
+            };
+        }
+
+        return Tomahawk.get(config.server + "stats", settings).then(function () {
+            return Tomahawk.ConfigTestResultType.Success;
+        }, function (xhr) {
+            if (xhr.status == 403) {
+                return Tomahawk.ConfigTestResultType.InvalidCredentials;
+            } else if (xhr.status == 404 || xhr.status == 0) {
+                return Tomahawk.ConfigTestResultType.CommunicationError;
+            } else {
+                return xhr.responseText.trim();
+            }
+        });
     }
 });
 
 Tomahawk.resolver.instance = BeetsResolver;
+
+var beetsCollection = Tomahawk.extend(Tomahawk.Collection, {
+    settings: {
+        id: "beets",
+        prettyname: "Beets",
+        description: BeetsResolver.server,
+        iconfile: "contents/images/icon.png",
+        trackcount: BeetsResolver.trackCount
+    }
+});
